7111812554f9ecded5d1479156d294aaacf7b075
rocknrollcoinradio <60813882+rocknrollcoinradio@users.noreply.github.com>
2020-02-18 21:46:23 +0200

    Update sticer.js

--- sticer.js
+++ sticer.js
@@ -5,31 +5,6 @@ const bot = new TelegramBot(token, {polling: true});
 var http = require('http'); 
 http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
 bot.on('new_chat_members', function(msg){
-    bot.sendMessage(msg.chat.id, 'Hello!', { reply_to_message_id: msg.message_id });
-});
-bot.on('message', (msg) => {
 	const chatId = msg.chat.id;// извлекаем id чата
-	const messageUsr = msg.from.username;
-	
-	if (msg.text) {
-		
-		const text = msg.text.toLowerCase();
-		
-		if (~text.indexOf("привет")) {  
-			bot.sendMessage(chatId, 'Приветик, ' + messageUsr + '!');
-			} else if (~text.indexOf("клав")) {
-			openKlava(chatId);
-			} else if (~text.indexOf("здраст")) {
-			bot.sendMessage(chatId, 'Здравствуй, здравствуй, ' + messageUsr + '!');
-			} else if (~text.indexOf("здравст")) {
-			bot.sendMessage(chatId, 'Здравствуй, здравствуй, ' + messageUsr + '!');
-			} else if (~text.indexOf("дур")) {
-			bot.sendMessage(chatId, '' + messageUsr + ', не ругайся, а то обижусь!');
-			} else if (~text.indexOf("туп")) {
-			bot.sendMessage(chatId, '' + messageUsr + ', не ругайся, а то обижусь!');
-			} else {
-			bot.sendMessage(chatId, '' + messageUsr + ', я тебя не понимать!');
-		}
-	}
-	bot.forwardMessage(chatId, idAdmin, msg.message_id);
+    bot.sendMessage(msg.chat.id, 'Hello!', { reply_to_message_id: msg.message_id });
 });
